7b601757be47b77e68fd861e09221b6007515372
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-08-31 23:53:54 -0400

    note on authenticate()

--- _db.js
+++ _db.js
@@ -4,6 +4,8 @@ var Sequelize = require('sequelize');
 var sequelize = new Sequelize('postgres://localhost:5432/sequelizedemo');
 //don't forget to  create the database in psql
 //# create database sequelizedemo;
+
+//authentication is mainly to test that you can connect
 sequelize
   .authenticate()
   .then(function(err) {
