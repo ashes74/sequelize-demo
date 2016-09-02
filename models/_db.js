b94e0630059da394beb90f6b5a877b17ec4772dd
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-09-01 20:37:38 -0400

    seed your file

--- models/_db.js
+++ models/_db.js
@@ -1,5 +1,5 @@
 // _db.js
-
+'use strict';
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/sequelizedemo');
 
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+'use strict';
 var Todo = require('./todo');
 var db = require('./_db');
 
