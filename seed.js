3aec42c4010d34b018af60b5a784283a0e251a36
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-09-01 21:23:45 -0400

    Preparing to do a query

--- seed.js
+++ seed.js
@@ -1,13 +1,22 @@
 'use strict';
 
 var models = require('./models'),
-  Todo = models.Todo
+  Todo = models.Todo,
   db = models.db;
 
 
 var tasks = [
   {
     task: "Make this the best presentation ever!"
+  },
+  {
+    task: "Sweep the porch"
+  },
+  {
+    task: "Make another bowl of popcorn"
+  },
+  {
+    task: "Pay off my student loans"
   }
 ]
 
@@ -16,6 +25,10 @@ console.log("-clearing-");
 db.sync({force:true})
 .then(function(){
   console.log('-done clearing-');
-  Todo.create(tasks[0])
-
+  console.log('-seeding tasks-');
+  return tasks.forEach(function(task){
+    Todo.create(task)
+  })
+}).then(function(){
+  
 })
